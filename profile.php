6f5ce66606ff4a24d2470fee7f15e4d408c34eb6
cameronmcnz <ubuntu-clone@example.com>
2023-03-09 23:48:34 +0530

    profile page changes

--- profile.php
+++ profile.php
@@ -11,6 +11,11 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   <link rel="stylesheet" href="navbarcss.css">
   <link rel="stylesheet" href="profilecss.css">
 </head>
@@ -35,7 +40,6 @@ session_start();
         <li>
           <?php if ($_SESSION['loggedin'] == true) {
             echo '<li><a href="" class="active">Profile</a></li>';
-            echo '<li><a href="add_product.php">Add product</a></li>';
             echo '<li><a href="logout.php">Logout</a></li>';
           } else {
             echo '<li><a href="login.php">Login</a></li>';
@@ -44,46 +48,60 @@ session_start();
       </ul>
     </nav>
   </header>
+  <!-- archan no code -->
 
-  <!-- profile page html -->
+  <div class="container-flex">
+            <div class="left-container">
+                <h2>hello</h2>
+                <h3><?php echo $_SESSION['username'] ?></h3>
+                <a class="logout" href="logout.php">Log Out</a>
+                <ul class="account-links">
 
-  <div class="main-container">
-    <div class="container1">
-      <img src="https://source.unsplash.com/400x400/?person" class="profile-img" alt="">
-      <div class="table-container">
-      <table class="profile-table">
-        <tr>
-          <td>NAME :</td>
-          <td>Priyanshu Prjapati</td>
-        </tr>
-        <tr>
-          <td>NAME :</td>
-          <td>Priyanshu Prjapati</td>
-        </tr>
-        <tr>
-          <td>NAME :</td>
-          <td>Priyanshu Prjapati</td>
-        </tr>
-        <tr>
-          <td>NAME :</td>
-          <td>Priyanshu Prjapati</td>
-        </tr>
-      </table>
-      </div>
-
-    </div>
-    <div class="container2">
-              <button class="profile-btns">ADD PRODUCT</button><br>
-              <button class="profile-btns">DELETE PRODUCT</button><br>
-              <button class="profile-btns">EDIT PROFILE</button><br>
-    </div>
-  </div>
-
-<!-- right buttons -->
-
-
-
+                    <li><a href="#" class="active">Dashboard</a></li>
+                    <?php if($_SESSION['role'] == "owner") {?>
+                    <li><a href="orders.php">Orders</a></li>
+                    <?php } ?>
+                    <li><a href="cart.php">Cart</a></li>
+                    <li><a href="products.php">Products</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="right-container">
+                <div class="cards">
+                    
+                    <div class="card-info">  
+                    <a class="card-body" href="add_product.php">
+                        <!-- <img src="myAccount.png" width="90x" height="90px"> -->
+                        <div class="details">
+                        <h2>ADD PRODUCT</h2>
+                        <p>Add new product </p>
+                    </div>
+                    </a>
+                    </div>
+                   
+                    <div class="card-info">  
+                    <a class="card-body" href="#">
+                        <!-- <img src="Address.png" width="90px" height="90px"> -->
+                        <div class="details">
+                        <h2>DELETE PRODUCT</h2>
+                        <p>Delete an exixting product</p>
+                    </div>
+                    </a>
+                    </div>
+                    <div class="card-info">  
+                    <a class="card-body" href="#">
+                        <!-- <img src="Address.png" width="90px" height="90px"> -->
+                        <div class="details">
+                        <h2>EDIT PROFILE</h2>
+                        <p>Edit your profile</p>
+                    </div>
+                    </a>
+                    </div>
+                   
+                </div>
+                <img src="wave.png" class="wave">
+            </div>
+        </div>
 
 </body>
-
 </html>
